c166f2d80c139af8e4c49c28c7e5362acfc2ab3a
Evgenia <semenutenkoevgenia@gmail.com>
2023-05-11 15:33:03 +0300

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$(".what-clients-saying-slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2
+  });
+/*
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });*/
